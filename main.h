02133f7580ad108e72b53396c7fa28e9ff93fde4
GGGtrainer <9716@holbertonstudents.com>
2025-03-29 01:14:37 -0400

    header faltan funciones por declarar

--- main.h
+++ main.h
@@ -1,10 +1,16 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef MAIN_H
+#define MAIN_H
+
 /*Libraries*/
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /*Prototypes*/
 int _printf(const char *format, ...);
+int _putchar();
+int print_digit();
+int _puts();
 
 #endif
