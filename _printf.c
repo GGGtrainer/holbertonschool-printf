fa47c66352d50f76dac4eb4142b5a0c8ceab991a
GGGtrainer <9716@holbertonstudents.com>
2025-03-29 01:13:59 -0400

    printf faltan descripciones

--- _printf.c
+++ _printf.c
@@ -1,25 +1,44 @@
 #include "main.h"
-#include <unistd.h>
-#include <stdarg.h>
-#include <stdlib.h>
 
 int _putchar(int c)
 {
-	return write(1, %c, 1);
+	return write(1, &c, 1);
 }
 
 int _puts(char *str)
 {
 	int count;
 
-	count = 0
+	count = 0;
 		while (*str != '\0')
 		{
 			_putchar((int)*str);
 			++count;
-			++str
+			++str;
 		}
 	return count;
+}
+
+int print_digit(long n, int base)
+{
+	int count;
+	char *symbols;
+
+	symbols = "0123456789abcdef";
+
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		return print_digit(-n, base) + 1;
+	}
+	else if (n < base)
+		return _putchar(symbols[n]);
+	else
+	{
+		count = print_digit(n / base, base);
+		return count + print_digit(n % base, base);
+	}
+
 }
 
 int print_format(char specifier, va_list argp)
@@ -32,9 +51,9 @@ int print_format(char specifier, va_list argp)
 	else if (specifier == 's')
 		count += _puts(va_arg(argp, char *));
 	else if (specifier == 'd')
-		count += print_digit(va_arg(argp, int), 10);
+		count += print_digit((long)va_arg(argp, int), 10);
 	else if (specifier == 'x')
-		count += print_digit(va_arg(arg, unsigned int), 16);
+		count += print_digit((long)va_arg(argp, unsigned int), 16);
 	else
 		count += write(1, &specifier, 1);
 	return count;
@@ -43,7 +62,8 @@ int print_format(char specifier, va_list argp)
 
 
 /**
- * _print -
+ * _printf - prints strings, chars, ints and unsigned ints
+ * return: count
  *
  */
 
@@ -73,6 +93,12 @@ int main()
 {
 	int count;
 
-	count = _printf("Hello there %s\n", "Anakin");
+	_printf("Hi %s, your bank statement is $%d, in hex %x, a char %c for your troubles\n", "Suge", 420, 420, 'w');
+/*	count = _printf("Hello there %s\n", "Anakin");
+	printf("Hello there %s\n", "Anakin");
+	count = _printf("%x\n", 0);
+	count = printf("%x\n", 0);
 	_printf("The chars written are %d\n", count);
+	printf("The chars written are %d\n", count);
+*/
 }
