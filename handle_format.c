56258784d7b121990e2e617c3ba3856b171900ce
Jcarrera007 <carrerasproductions@gmail.com>
2025-03-31 01:52:05 -0400

    fixed betty issue

---
+++ handle_format.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * handle_format - handles format specifiers for _printf
+ * @format: the format string
+ * @args: va_list of arguments
+ * @i: pointer to the current index in the format string
+ *
+ * Return: number of characters printed
+ */
+int handle_format(const char *format, va_list args, int *i)
+{
+	int count = 0;
+
+	(*i)++;
+
+	if (format[*i] == 'd' || format[*i] == 'i')
+	{
+		count += print_int(va_arg(args, int));
+	}
+	else if (format[*i] == 'c')
+	{
+		count += _putchar(va_arg(args, int));
+	}
+	else if (format[*i] == 's')
+	{
+		char *str = va_arg(args, char *);
+		int j = 0;
+
+		if (!str)
+			str = "(null)";
+		while (str[j])
+		{
+			count += _putchar(str[j]);
+			j++;
+		}
+	}
+	else if (format[*i] == '%')
+	{
+		count += _putchar('%');
+	}
+	else
+	{
+		count += _putchar('%');
+		count += _putchar(format[*i]);
+	}
+
+	return (count);
+}
+
--- main.h
+++ main.h
@@ -11,5 +11,6 @@
 int _printf(const char *format, ...);
 int _putchar(int c);
 int print_int(int base);
+int handle_format(const char *format, va_list args, int *i);
 
 #endif
