735a8886f15295e32bd12011def9660d82eebffb
Jcarrera007 <carrerasproductions@gmail.com>
2025-03-30 23:36:27 -0400

    reverted

--- _printf.c
+++ _printf.c
@@ -18,47 +18,43 @@ int _printf(const char *format, ...)
 	while (format[i])
 	{
 		if (format[i] == '%' && format[i + 1])
-		{
-			va_end(args);
-			return (-1);
-		}
-		{
-			i++;
-			if (format[i] == 'd' || format[i] == 'i')
-			{
-				count += print_int(va_arg(args, int));
-			}
-			else if (format[i] == 'c')
-			{
-				count += _putchar(va_arg(args, int));
-			}
-			else if (format[i] == 's')
-			{
-				char *str = va_arg(args, char *);
-				int j = 0;
 
-				if (!str) str = "(null)";
-				while (str[j])
-					count += _putchar(str[j++]);
-			}
-			else if (format[i] == '%')
-			{
-				count += _putchar('%');
-			}
-			else
-			{
-				count += _putchar('%');
-				count += _putchar(format[i]);
-			}
+			i++;
+		if (format[i] == 'd' || format[i] == 'i')
+		{
+			count += print_int(va_arg(args, int));
+		}
+		else if (format[i] == 'c')
+		{
+			count += _putchar(va_arg(args, int));
+		}
+		else if (format[i] == 's')
+		{
+			char *str = va_arg(args, char *);
+			int j = 0;
+
+			if (!str) str = "(null)";
+			while (str[j])
+				count += _putchar(str[j++]);
+		}
+		else if (format[i] == '%')
+		{
+			count += _putchar('%');
 		}
 		else
 		{
+			count += _putchar('%');
 			count += _putchar(format[i]);
 		}
-		i++;
 	}
+	else
+	{
+		count += _putchar(format[i]);
+	}
+	i++;
+}
 
-	va_end(args);
-	return (count);
+va_end(args);
+return (count);
 }
 
