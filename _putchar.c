eaf751e64d4cc7fb7a32cc2b6df18edd276ae5ec
Jcarrera007 <carrerasproductions@gmail.com>
2025-03-31 02:24:47 -0400

    fix putchar problem and betty doc style

--- _putchar.c
+++ _putchar.c
@@ -1,8 +1,8 @@
-#include <unistd.h>
-
 /**
- * _putchar - writes the character c to stdout
- * Return: On success 1, -1 on error
+ * _putchar - Writes a character to stdout
+ * @c: The character to print
+ *
+ * Return: On success, 1. On error, -1 is returned.
  */
 int _putchar(char c)
 {
